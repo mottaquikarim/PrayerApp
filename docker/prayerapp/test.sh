ed91e52c4459963f105f9357b7f312f97a2dee16
Mottaqui Karim <taqqui.karim@gmail.com>
2018-05-21 06:22:19 -0400

    moved cc hooks into docker test in order to ensure test results can be captured

--- docker/prayerapp/test.sh
+++ docker/prayerapp/test.sh
@@ -2,7 +2,20 @@
 
 set -exo pipefail
 
+# Only run if CC_TEST_REPORTER_ID is set
+[ -z "${CC_TEST_REPORTER_ID}" ] || {
+  # CodeClimate requires GIT_COMMITTED_AT to be set
+  # ( when it can't read this from .git )
+  # I'm not sure how it's actually used
+  # or if making it up is bad in any way
+  GIT_COMMITTED_AT=$(date +%s)
+  export GIT_COMMITTED_AT
+  cc-test-reporter before-build
+} || true
+
 pytest ./test
 EXIT_CODE=$?
+
+[ -z "${CC_TEST_REPORTER_ID}" ] || cc-test-reporter after-build --debug --exit-code ${EXIT_CODE} || true
 
 exit ${EXIT_CODE}
