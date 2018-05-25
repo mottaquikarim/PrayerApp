0947f916d275022e50cea76474c8c508034d616f
Mottaqui Karim <taqqui.karim@gmail.com>
2018-05-25 05:19:20 -0400

    refactored google apis code to use classes and inheritance

--- src/prayerapp/google_apis.py
+++ src/prayerapp/google_apis.py
@@ -37,28 +37,23 @@ class ParamValidType(GValidator):
 class ParamValidFmt(GValidator):
     exception_str = 'Required param {} does not match format {}'
 
-    def validate_val_keys(self):
-        if not self.val.get('matches') or not self.val.get('type') == str:
-            raise GValidationException('type and format required')
-
     def validate_val_matches(self):
+        if not self.val.get('matches'):
+            return
+
         r = re.compile(self.val.get('matches'))
         if not r.match(self.param):
             raise GValidationException(self.exception_str
                                        .format(self.name, self.val.get('matches')))
 
     def validate(self):
-        self.validate_val_keys()
         self.validate_val_matches()
 
 
 class GoogleAPICall(object):
     URL_BASE = "https://maps.googleapis.com"
 
-    def __init__(self, api_key=None, params=None, required_params=None):
-        if not api_key:
-            raise Exception({"error": "api_key required", })
-
+    def __init__(self, api_key, params=None, required_params=None):
         self.params = params or {}
         self.params.update({'api_key': api_key})
         self.required_params = required_params or {}
@@ -67,7 +62,7 @@ class GoogleAPICall(object):
         self.usable_params = self.paramify()
 
     def paramify(self):
-        return '&'.join(["%s=%s" % (x, self.params[x]) for x in self.params.keys()])
+        return '&'.join(["%s=%s" % (x, self.params[x]) for x in sorted(self.params.keys())])
 
     def get_endpoint(self, endpoint):
         return "{base}{endpoint}?{params}".format(base=self.URL_BASE,
@@ -84,91 +79,37 @@ class GoogleAPICall(object):
                 validator(param, rparam_name, rparam_val).validate()
 
     def do_api_call(self, endpoint):
-        try:
-            return requests.get(self.get_endpoint(endpoint)).json()
-        except requests.RequestException as e:
-            raise Exception({'error': 'Failed to geocode latitude/longitude', })
+        return requests.get(self.get_endpoint(endpoint)).json()
 
 
 class GoogleQueryEndpoint(GoogleAPICall):
 
     def __init__(self, endpoint, *args, **kwargs):
-        super.__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
         self.endpoint = endpoint
 
     def query(self):
         return self.do_api_call(self.endpoint)
 
 
-class GoogleAPIs(object):
-    URL_BASE = "https://maps.googleapis.com"
+class Geocode(GoogleQueryEndpoint):
+    endpoint = '/maps/api/geocode/json'
+    required_params = {
+        'location': {'type': str, 'matches': '.*,.*'},
+        'timestamp': {'type': int},
+    }
 
-    @staticmethod
-    def get_params(params):
-        if not params:
-            params = {}
+    def __init__(self, api_key, params):
+        super().__init__(self.endpoint, api_key, params=params,
+                         required_params=self.required_params)
 
-        return params
 
-    @staticmethod
-    def paramify(params=None):
-        params = GoogleAPIs.get_params(params)
-        return '&'.join(["%s=%s" % (x, params[x]) for x in params.keys()])
+class GetTimezone(GoogleQueryEndpoint):
+    endpoint = '/maps/api/timezone/json'
+    required_params = {
+        'address': {'type': str, 'matches': '.*,.*'},
+    }
 
-    @staticmethod
-    def validate_params(api_key=None, params=None, required_params=None):
-        print(api_key, params, required_params)
-        params = GoogleAPIs.get_params(params)
-        if not api_key:
-            raise Exception({"error": "api_key required", })
-        else:
-            params.update({'api_key': api_key})
-
-        if not required_params:
-            required_params = {}
-
-        for rparam_name, rparam_val in required_params.items():
-            param = params.get(rparam_name)
-            if not param:
-                raise Exception('Required param {} not provided'.format(rparam_name))
-
-            if not isinstance(param, rparam_val.get('type')):
-                raise Exception('Required param {} is not correct type, expected {} got {}'
-                                .format(rparam_name, rparam_val.get('type'), type(param)))
-
-            if rparam_val.get('matches') and rparam_val.get('type') == str:
-                r = re.compile(rparam_val.get('matches'))
-                if not r.match(param):
-                    raise Exception('Required param {} does not match format {}'
-                                    .format(rparam_name, rparam_val.get('matches')))
-
-        return params
-
-    @staticmethod
-    def do_api_call(endpoint, api_key=None, params=None, required_params=None):
-        params = GoogleAPIs.get_params(params)
-        usable_params = GoogleAPIs.paramify(GoogleAPIs.validate_params(api_key=api_key,
-                                                                       params=params, required_params=required_params))
-        url = "{base}{endpoint}?{params}".format(base=GoogleAPIs.URL_BASE,
-                                                 endpoint=endpoint, params=usable_params)
-
-        try:
-            return requests.get(url).json()
-        except requests.RequestException as e:
-            raise Exception({'error': 'Failed to geocode latitude/longitude', })
-
-    @staticmethod
-    def geocode(api_key=None, params=None):
-        return GoogleAPIs.do_api_call('/maps/api/geocode/json',
-                                      api_key=api_key, params=GoogleAPIs.get_params(params),
-                                      required_params={'location': {'type': str, 'matches': '.*,.*'},
-                                                       'timestamp': {'type': int},
-                                                       })
-
-    @staticmethod
-    def get_timezone(api_key=None, params=None):
-        return GoogleAPIs.do_api_call('/maps/api/timezone/json',
-                                      api_key=api_key, params=GoogleAPIs.get_params(params),
-                                      required_params={
-                                          'address': {'type': str, 'matches': '.*,.*'},
-                                      })
+    def __init__(self, api_key, params):
+        super().__init__(self.endpoint, api_key, params=params,
+                         required_params=self.required_params)
--- test/test_google_api_call.py
+++ test/test_google_api_call.py
@@ -1,10 +1,13 @@
 import os
 import pytest
+from types import SimpleNamespace
 
+from requests import RequestException
 from unittest import TestCase
 from unittest.mock import patch
 
-from prayerapp.google_apis import GoogleAPICall, GoogleQueryEndpoint,\
+from prayerapp.google_apis import GetTimezone, Geocode,\
+    GoogleAPICall, GoogleQueryEndpoint,\
     GValidationException, GValidator, ParamExists, ParamValidType, \
     ParamValidFmt
 
@@ -53,8 +56,7 @@ class TestParamValidFmt(TestCase):
             gv.validate()
 
         gv2 = ParamValidFmt('test', 'location', {})
-        with pytest.raises(GValidationException) as e:
-            gv2.validate()
+        gv2.validate()
 
         gv.val = {'type': str, 'matches': '.*,.*'}
         gv.param = 'a,b'
@@ -64,9 +66,94 @@ class TestParamValidFmt(TestCase):
 class TestGoogleAPICall(object):
 
     def test_init(self):
-        with pytest.raises(Exception) as e:
-            g = GoogleAPICall()
 
-        g = GoogleAPICall(api_key='test')
+        g = GoogleAPICall('test')
         assert g.params == {'api_key': 'test'}
         assert g.required_params == {}
+
+    def test_get_endpoint(self):
+        g = GoogleAPICall('test', params={
+            'test': 1,
+        })
+
+        endpt = g.get_endpoint('/test_endpoint')
+        assert endpt == 'https://maps.googleapis.com/test_endpoint?api_key=test&test=1'
+
+    def test_validate_params(self):
+        g = GoogleAPICall('test', params={
+            'test': 1,
+        })
+
+        # assert that if required params not present
+        # we get back input params mutated with api_key
+        g.validate_params()
+        assert g.params == {'test': 1, 'api_key': 'test'}
+
+        # assert error raised if required param does not exist
+        with pytest.raises(GValidationException) as e:
+            g = GoogleAPICall('test', params={
+                'test': 1,
+            }, required_params={
+                'test': {'type': str},
+                'timestamp': {'type': int},
+            })
+
+        # assert no errors raised if we pass in required params
+        # and params that match requirements
+        g = GoogleAPICall('test', params={
+            'test': '1',
+            'timestamp': 2,
+        }, required_params={
+            'test': {'type': str, 'matches': '\d'},
+            'timestamp': {'type': int},
+        })
+
+        assert g.params == {'test': '1', 'api_key': 'test', 'timestamp': 2}
+
+
+class TestGoogleQueryEndpoint(TestCase):
+
+    def test_init(self):
+        gq = GoogleQueryEndpoint('/test_endpoint', 'test', params={
+            'test': '1',
+            'timestamp': 2,
+        }, required_params={
+            'test': {'type': str, 'matches': '\d'},
+            'timestamp': {'type': int},
+        })
+
+        assert gq.endpoint == '/test_endpoint'
+        assert gq.params == {'test': '1', 'api_key': 'test', 'timestamp': 2}
+
+    @patch('prayerapp.google_apis.requests.get')
+    def test_query(self, MockRequestsGet):
+        def _getJson(*a, **kw):
+            return {}
+
+        MockRequestsGet.return_value = SimpleNamespace(json=_getJson)
+        gq = GoogleQueryEndpoint('/test_endpoint', 'test', params={
+            'test': '1',
+            'timestamp': 2,
+        }, required_params={
+            'test': {'type': str, 'matches': '\d'},
+            'timestamp': {'type': int},
+        })
+
+        assert gq.query() == {}
+
+
+class TestGeocode(TestCase):
+
+    def test_geocode(self):
+        Geocode('test', {
+            'location': 'a,b',
+            'timestamp': 2,
+        })
+
+
+class TestGetTimezone(TestCase):
+
+    def test_gettimezone(self):
+        GetTimezone('test', {
+            'address': 'a,b',
+        })
